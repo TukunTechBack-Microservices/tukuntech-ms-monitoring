5f886f93245eaf53971942d96688d8e9c4cd8981
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-27 10:47:12 -0500

    chore: initial commit

---
+++ src/test/java/com/upc/tukuntechmsmonitoring/TukuntechMsMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechmsmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukuntechMsMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
